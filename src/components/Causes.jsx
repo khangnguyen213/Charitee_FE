a7cf619d4f3230c1f8d3d9b24c9bd036c3119b03
khangnguyen213 <khangnguyenm01@gmail.com>
2023-06-03 09:18:55 +0700

    refix image

--- src/components/Causes.jsx
+++ src/components/Causes.jsx
@@ -53,14 +53,10 @@ export const Causes = (props) => {
             causes.map((cause) => {
               return (
                 <div className="mb-6 lg:mb-0 " key={cause._id}>
-                  <div className="relative flex flex-col justify-between h-[600px] bg-[#F4F2F1] rounded-lg ">
+                  <div className="relative bg-[#F4F2F1] rounded-lg ">
                     <div className="flex">
                       <div className="relative h-[264px] overflow-hidden bg-no-repeat bg-cover rounded-lg mx-4 -mt-4">
-                        <img
-                          src={cause.image}
-                          alt="cause"
-                          className="w-full h-full"
-                        />
+                        <img src={cause.image} alt="cause" className="w-full" />
                         <button
                           onClick={() => navigate(`/donate/${cause._id}`)}
                         >
