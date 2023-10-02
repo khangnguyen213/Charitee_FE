c7edddf5da123ffb79a6bd7e150af5e8e8a2aca1
khangnguyen213 <khangnguyenm01@gmail.com>
2023-10-02 19:52:38 +0700

    [Admin] Update UI

--- src/components/AdminDashboard/CauseLineChart.jsx
+++ src/components/AdminDashboard/CauseLineChart.jsx
@@ -7,6 +7,7 @@ import {
   Title,
   Tooltip,
   Legend,
+  Filler,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { filterCountCauseData } from '../../utils/filterData';
@@ -17,7 +18,8 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  Filler
 );
 
 const CauseLineChart = (props) => {
